5c630d1db5f3faf1cb66060bab2e6090d5749b10
“sowmalya <sowmamlya@gmail.com>
2026-09-23 10:14:21 -0400

    initial code added

---
+++ new.js
@@ -0,0 +1,28 @@
+function generatePrimes(n) {
+    const primes = [];
+    let num = 5;
+    
+    while (primes.length < n) {
+        if (isPrime(num)) {
+            primes.push(num);
+        }
+        num++;
+    }
+    
+    return primes;
+}
+
+function isPrime(num) {
+    if (num < 2) return false;
+    if (num === 2) return true;
+    if (num % 2 === 0) return false;
+    
+    for (let i = 3; i <= Math.sqrt(num); i += 2) {
+        if (num % i === 0) return false;
+    }
+    
+    return true;
+}
+
+// Usage: Generate next 10 prime numbers
+console.log(generatePrimes(10));
